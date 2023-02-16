3a29f90ab15dd2c54bef85f34204a26f36be9d87
Kristina Krutova <fruktovaya99@mail.ru>
2023-02-17 00:43:12 +0300

    Add OOP logic of class Post

---
+++ src/main/java/principles/Post.java
@@ -0,0 +1,11 @@
+package principles;
+
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public Boolean subscription;
+    public FormDate birthday;
+}
